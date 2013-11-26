289fa704caf8f6bd6cad8b17ef759874ac16f760
ogawa <ogawa@r-up.jp>
2013-11-26 10:27:14 +0900

    first commit

---
+++ TwitterClient/TwitterClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterClient
+//
+//  Created by yukichang on 2013/11/26.
+//  Copyright (c) 2013年 yukichang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
